bfaa6620af4437d7b60493717d3ef23730a3c986
Winlypse <nts3@o2.pl>
2025-07-06 20:32:24 +0200

    code!

--- src/main/kotlin/winlyps/lavaFishingRod/LavaFishingRod.kt
+++ src/main/kotlin/winlyps/lavaFishingRod/LavaFishingRod.kt
@@ -1,11 +1,28 @@
 package winlyps.lavaFishingRod
 
+import org.bukkit.Material
+import org.bukkit.entity.FishHook
 import org.bukkit.plugin.java.JavaPlugin
 
 class LavaFishingRod : JavaPlugin() {
 
+    private lateinit var fishingListener: FishingListener
+
     override fun onEnable() {
-        // Plugin startup logic
+        fishingListener = FishingListener()
+        server.pluginManager.registerEvents(fishingListener, this)
+
+        server.scheduler.runTaskTimer(this, Runnable {
+            for (world in server.worlds) {
+                for (entity in world.entities) {
+                    if (entity is FishHook) {
+                        if (entity.location.block.type == Material.LAVA) {
+                            fishingListener.addLavaHook(entity)
+                        }
+                    }
+                }
+            }
+        }, 0L, 1L) // Check every tick
     }
 
     override fun onDisable() {
